c87ee8ead92dc5b9d7e69bffa47d55b486cd963e
Sghen <945654877@qq.com>
2021-05-31 21:04:24 +0800

    [!]初始化

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div ref="container" class="canvas-wrapper"></div>
+  <div class="menus-wapper"></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import SgHouse from "./core/index.ts";
+
+// import "./core/index1.js";
+
+export default defineComponent({
+  name: "App",
+
+  setup() {},
+
+  mounted() {
+    window.app = this;
+    this.init();
+  },
+
+  methods: {
+    init() {
+      this.sgHouse = new SgHouse(this.$refs.container);
+      window.sgHouse = this.sgHouse;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.canvas-wrapper {
+  height: 100%;
+}
+
+.menus-wapper {
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: transparent;
+  pointer-events: none;
+}
+</style>
---
+++ src/core/index.ts
@@ -0,0 +1,74 @@
+import {
+  Scene, WebGLRenderer, OrthographicCamera,
+  AxesHelper, GridHelper,
+} from "three";
+import { OrbitControls } from './orbitcontrols';
+
+export default class SgHouse {
+
+  renderer: WebGLRenderer;
+  camera: OrthographicCamera;
+  scene: Scene;
+
+  controls: OrbitControls;
+
+  /**
+   * 画面缩放比为画布容器clientWidth / clientHeight
+   * @param container 画布容器
+   */
+  constructor(container: HTMLDivElement) {
+    const { clientWidth, clientHeight } = container;
+
+    const renderer = new WebGLRenderer({ antialias: true });
+    renderer.setSize(clientWidth, clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.appendChild(renderer.domElement);
+    this.renderer = renderer;
+
+    const aspect = clientWidth / clientHeight;
+    const camera = new OrthographicCamera(-aspect, aspect, 1, -1, 0.01, 10);
+    camera.lookAt(0, 0, 0);
+    camera.position.set(0, 0, 5);
+    this.camera = camera;
+
+    const controls = new OrbitControls(camera, this.renderer.domElement);
+    controls.minZoom = 0.2;
+    controls.maxZoom = 5;
+    // 上下旋转范围
+    controls.minPolarAngle = Math.PI * 1 / 10;
+    controls.maxPolarAngle = Math.PI * 4 / 10;
+    // 左右旋转范围
+    // controls.minAzimuthAngle = -Math.PI;
+    // controls.maxAzimuthAngle = Math.PI;
+    controls.update();
+    this.controls = controls;
+
+    this.scene = new Scene();
+    this.animate();
+
+    this.addAxesGrid()
+  }
+
+  /**
+   * 添加坐标轴和辅助网格
+   */
+  addAxesGrid() {
+    const grid = new GridHelper(10, 10, 0x999999, 0x999999);
+    this.scene.add(grid);
+
+    const axesHelper = new AxesHelper(10);
+    axesHelper.position.y = 0.001;
+    this.scene.add(axesHelper);
+  }
+
+  /**
+   * 更新画面
+   */
+  animate() {
+    requestAnimationFrame(() => {
+      this.animate();
+    })
+
+    this.renderer.render(this.scene, this.camera);
+  }
+}
