ce38f7d538fdaa53e6c4196001673a7d1e5772b8
Sghen <945654877@qq.com>
2021-06-01 11:12:18 +0800

    [*]优化wall参数

--- src/core/entities/wall.ts
+++ src/core/entities/wall.ts
@@ -2,9 +2,11 @@ import { BoxGeometry, MeshBasicMaterial, Mesh } from "three";
 
 export default class Wall extends Mesh {
 
-  constructor(width: number = 3, height: number = 4, depth: number = 0.1) {
+  constructor(width: number = 4, height: number = 3, depth: number = 0.1) {
     const geometry = new BoxGeometry(width, height, depth);
     const material = new MeshBasicMaterial({ color: 0xaaaaaa });
     super(geometry, material)
+
+    this.position.setY(height / 2);
   }
 }
